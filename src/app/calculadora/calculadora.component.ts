6b7e6d6dbc5fcc50fe899551d0290bad49568575
ArielDBV <balderramaarieldavid@gmail.com>
2024-04-30 00:49:28 -0300

    arreglando errores

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,26 +1,19 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { Sumadora } from '../sumadora';
 
 @Component({
   selector: 'app-calculadora',
   standalone: true,
-  imports[FormsModule];
+  imports:[FormsModule],
   templateUrl: './calculadora.component.html',
   styleUrl: './calculadora.component.css'
 })
 export class CalculadoraComponent {
-  public numero2:number;
-  public numero1:number;
-  public resultado:number | undefined ;
-  public calcular:number;
+  sumadora:Sumadora;
+
   constructor(){
-    this.numero1 = 0;
-    this.numero2 = 0;
+    this.sumadora = new Sumadora();
   }
-  calcular(){
-    this.resultado = this.numero1 + this.numero2;
-
-  }
-
+  
 }
